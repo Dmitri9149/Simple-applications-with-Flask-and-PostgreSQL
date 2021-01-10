bd31cdc1f8f7cbc7e4c697b147f102047d232d35
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-10 18:38:42 +0200

    send name by form, get result

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -30,3 +30,11 @@ def page():
 @app.route("/pictures")
 def pictures():
     return render_template("pictures.html", message="Some picture made by Haskell")
+
+@app.route("/form")
+def form():
+    return render_template("/form.html")
+@app.route("/result", methods=["POST"])
+def result(): 
+    return render_template("result.html", name = request.form["name"])
+
