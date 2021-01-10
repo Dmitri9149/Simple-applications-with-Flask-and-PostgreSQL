5a74fde5cd6791cf13746a3c14c1df91b8812197
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-10 15:47:36 +0200

    some routes/responses

--- app.py
+++ app.py
@@ -4,5 +4,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return "<b>Tervetuloa</b> <i>sovellukseen</i>"
+
+@app.route("/page1")
+def page1():
+    return "Tämä on Page1"
+
+@app.route("/page2")
+def page2():
+    return "Tama on Page2"
+
+@app.route("/test")
+def test():
+    content = ""
+    for i in range(1,101):
+        content += str(i)+" "
+    return content
+
+@app.route("/page/<int:id>")
+def page():
+    return "Tama on sive "+str(id)
+
 
